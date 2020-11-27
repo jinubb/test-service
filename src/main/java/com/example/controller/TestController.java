a2686959517407a42c8f619a42b78b7fefbf0352
contact contact <contact@ai-car.co.kr>
2020-11-27 14:53:18 +0900

    Jenkins test-service

---
+++ src/main/java/com/example/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping("/test/jenkins")
+public class TestController {
+	@ApiOperation(value = "테스트 메소드")
+	@GetMapping(value = "/")
+	public String testMethod() {
+		return "Hello jenkins!";
+	}
+}
